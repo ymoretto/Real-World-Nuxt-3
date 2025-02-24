62eab81ae83af23a0748841086f04dff113cc582
Yolanda Scudeller <54295639+ymoretto@users.noreply.github.com>
2025-02-24 16:20:47 +0100

    Create [post].vue

---
+++ pages/posts/[post].vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const postSlug = useRoute().params.post;
+</script>
+
+<template>
+    <main>
+        <h1>
+            Nuxt Post #1
+        </h1>
+    </main>
+</template>
